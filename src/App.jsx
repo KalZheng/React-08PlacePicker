4f989100eda150c25de35325634691dfd12b92e8
Kal Zheng <kal.zheng@inspire-dt.com>
2023-12-12 16:08:02 +0800

    add timer in delete confirmation, with useeffejects.ct, and adding some security with call back to make sure infinite loop is not occure when function is use as a dependencies in useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 import Places from "./components/Places.jsx";
 import { AVAILABLE_PLACES } from "./data.js";
@@ -14,7 +14,6 @@ const storedPlaces = storedIds.map((id) =>
 );
 
 function App() {
-
   const selectedPlace = useRef();
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
@@ -70,8 +69,7 @@ function App() {
       );
     }
   }
-
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
@@ -82,7 +80,7 @@ function App() {
       "selectedPlaces",
       JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
     );
-  }
+  }, []);
 
   return (
     <>

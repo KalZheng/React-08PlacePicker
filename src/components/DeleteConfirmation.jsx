d9cfbaac15fbc7c3f9cf144eeb0a996e2b358de3
Kal Zheng <kal.zheng@inspire-dt.com>
2023-12-12 16:22:16 +0800

    add progress bar and interval to show how much remaining tme to click before close modal

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,21 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  const [remainingTime, setRemainingTime] = useState(TIMER);
+
   useEffect(() => {
-    console.log('timer');
+    const interval = console.log("Timer interval");
+    setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 10);
+    }, 10);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("timer");
     const timer = setTimeout(() => {
       onConfirm();
-    }, 3000);
+    }, TIMER);
 
     return () => {
       clearTimeout(timer);
     };
   }, [onConfirm]);
-  //onConfirm is from app, 
-  // but function is also an object (javascript behavior) 
-  // so if app gets run again 
+  //onConfirm is from app,
+  // but function is also an object (javascript behavior)
+  // so if app gets run again
   // this function will be keep running (8 loop)
-  // hence u will need a useCallback in app 
+  // hence u will need a useCallback in app
   // to wrap around the function i.e handleRemovePlace
 
   return (
@@ -30,6 +45,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <progress value={remainingTime} max={TIMER} />
     </div>
   );
 }

a0dc081f69fa63597e5849c01e1c2a7cb7ea9e17
Kal Zheng <kal.zheng@inspire-dt.com>
2023-12-12 15:08:01 +0800

    add another use effect that does not use use effect function

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,6 @@ function App() {
     });
   }, []);
 
-
   function handleStartRemovePlace(id) {
     modal.current.open();
     selectedPlace.current = id;
@@ -45,6 +44,14 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem(
+        "selectedPlaces",
+        JSON.stringify([id, ...storedIds])
+      );
+    }
   }
 
   function handleRemovePlace() {
